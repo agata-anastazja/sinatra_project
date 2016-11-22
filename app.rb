826d23d9351f4c2a1bd6cbbd44d275dbdbd1a3b3
Rob Adams <robert.andrew.adams@gmail.com>
2016-11-22 12:09:08 +0000

    added cat image

--- app.rb
+++ app.rb
@@ -1,5 +1,4 @@
 require 'sinatra'
-# require 'shotgun'
 
 get '/hello/' do
   "<h1> Hello ROB! </h1>"
@@ -11,4 +10,10 @@ end
 
 get '/Bananas/' do
   "<h1>Seriously, No Bananas</h1>"
+end
+
+get '/cat' do
+  "<img src='http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png'
+  style='border:dashed red'
+  >"
 end
